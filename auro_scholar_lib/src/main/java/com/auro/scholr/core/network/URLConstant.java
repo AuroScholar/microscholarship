7195e40894e9813be968ebd89e3e2c59214c2f7f
Your Name <you@example.com>
2020-05-18 12:22:11 +0530

    update url in https and change language in hindi on 18/05/2020

--- auro_scholar_lib/src/main/java/com/auro/scholr/core/network/URLConstant.java
+++ auro_scholar_lib/src/main/java/com/auro/scholr/core/network/URLConstant.java
@@ -3,7 +3,7 @@ package com.auro.scholr.core.network;
 
 public interface URLConstant {
 
-    String BASE_URL = "http://auroscholar.com/api/"; // PRODUCTION
+    String BASE_URL = "https://auroscholar.com/api/"; // PRODUCTION
     // String BASE_URL = "http://3.210.192.55:6060/"; // UAT
 //    String BASE_URL = "http://14.142.204.99:7070/"; // SIT
     // String BASE_URL = BuildConfig.BASE_URL;
@@ -18,7 +18,7 @@ public interface URLConstant {
     String UPLOAD_IMAGE_URL = BASE_URL + "kyc.php";
     String GET_ASSIGNMENT_ID = BASE_URL + "statrquizeaction.php ";
     String TEST_URL = "https://assessment.eklavvya.com/exam/StartExam?";
-    String PRIVACY_POLICY = "http://auroscholar.com/privacy_policy.php";
+    String PRIVACY_POLICY = "https://auroscholar.com/privacy_policy.php";
 
 
 }
