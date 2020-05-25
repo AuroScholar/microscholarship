21cdce92c91098355db8ae3b3b8499ecf8a39ac1
varun <vksep07@gmail.com>
2020-05-25 20:25:42 +0530

    25-May-2020,Changes forerrors

--- auro_scholar_lib/src/main/java/com/auro/scholr/util/AuroScholar.java
+++ auro_scholar_lib/src/main/java/com/auro/scholr/util/AuroScholar.java
@@ -1,22 +1,11 @@
 package com.auro.scholr.util;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 
-import com.auro.scholr.R;
 import com.auro.scholr.core.application.AuroApp;
-import com.auro.scholr.core.common.AppConstant;
-import com.auro.scholr.core.common.FragmentUtil;
 import com.auro.scholr.home.data.model.AuroScholarDataModel;
 import com.auro.scholr.home.data.model.AuroScholarInputModel;
-import com.auro.scholr.home.presentation.view.activity.HomeActivity;
 import com.auro.scholr.home.presentation.view.fragment.QuizHomeFragment;
-import com.auro.scholr.home.presentation.view.fragment.ScholarShipFacebookIn;
-import com.auro.scholr.home.presentation.view.fragment.ScholarShipFragment;
 
 public class AuroScholar {
 
