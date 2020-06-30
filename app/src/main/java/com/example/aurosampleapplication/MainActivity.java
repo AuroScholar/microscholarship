e0f61242b84c6adce569d7742d8aea64deb31fc5
Your Name <you@example.com>
2020-06-30 13:12:27 +0530

    add fb share app id on 30/06/2020

--- app/src/main/java/com/example/aurosampleapplication/MainActivity.java
+++ app/src/main/java/com/example/aurosampleapplication/MainActivity.java
@@ -71,9 +71,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.bt_sdk:
-              openTeacherSDK();
+
+                openTeacherSDK();
              //  openGenricSDK();
-               // openScholarSpecificSdk();
+             //   openScholarSpecificSdk();
                 hideKeyboard(this);
 
 
