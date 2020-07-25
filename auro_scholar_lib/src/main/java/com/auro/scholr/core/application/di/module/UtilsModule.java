4e93ea529c070db2d6633c6ddddacbff5684fff6
varun <vksep07@gmail.com>
2020-07-25 15:20:56 +0530

    Chnages for log disable

--- auro_scholar_lib/src/main/java/com/auro/scholr/core/application/di/module/UtilsModule.java
+++ auro_scholar_lib/src/main/java/com/auro/scholr/core/application/di/module/UtilsModule.java
@@ -51,7 +51,7 @@ public class UtilsModule {
     OkHttpClient getRequestHeader(HttpLoggingInterceptor httpLoggingInterceptor) {
 
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-        httpClient.addInterceptor(httpLoggingInterceptor);
+       // httpClient.addInterceptor(httpLoggingInterceptor);
 
         httpClient.addInterceptor(chain -> {
             Request original = chain.request();
