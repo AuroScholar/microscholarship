a6fcacf8addcda5627720d42189eff46ed853404
varun <vksep07@gmail.com>
2020-10-07 10:44:25 +0530

    Chnages for min sdk 16

--- app/src/main/java/com/example/aurosampleapplication/MainActivity.java
+++ app/src/main/java/com/example/aurosampleapplication/MainActivity.java
@@ -177,7 +177,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         inputModel.setStudentClass(student_class);
         inputModel.setRegitrationSource("AuroScholr");
         inputModel.setReferralLink("");
-        inputModel.setPartnerSource("SCHO34kyd8"); //this id is provided by auroscholar for valid partner
+        inputModel.setPartnerSource("Id is provided by the auroscholar"); //this id is provided by auroscholar for valid partner
         inputModel.setActivity(this);
         inputModel.setFragmentContainerUiId(R.id.home_container);
         openFragment(AuroScholar.startAuroSDK(inputModel));
