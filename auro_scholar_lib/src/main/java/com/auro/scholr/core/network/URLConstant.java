6a1836374b1a084a043316790eccaad2af43a945
Your Name <you@example.com>
2020-11-04 14:29:39 +0530

    partner source change on 04/11/2020

--- auro_scholar_lib/src/main/java/com/auro/scholr/core/network/URLConstant.java
+++ auro_scholar_lib/src/main/java/com/auro/scholr/core/network/URLConstant.java
@@ -3,8 +3,8 @@ package com.auro.scholr.core.network;
 public interface URLConstant {
 
 
-    //String BASE_URL = "https://auroscholar.com/api/"; // PRODUCTION/UAT
-    String BASE_URL = "http://demo.auroscholar.com/api/";  // Dev /SIT
+    String BASE_URL = "https://auroscholar.com/api/"; // PRODUCTION/UAT
+    //String BASE_URL = "http://demo.auroscholar.com/api/";  // Dev /SIT
 
     // String DASHBOARD_API = BASE_URL + "dashboard.php";
 
