1256a6ad8d63cbd520bb031da7675185f3606eab
Your Name <you@example.com>
2020-11-07 15:07:55 +0530

    minoor bug solve on

--- auro_scholar_lib/src/main/java/com/auro/scholr/home/presentation/view/activity/CameraActivity.java
+++ auro_scholar_lib/src/main/java/com/auro/scholr/home/presentation/view/activity/CameraActivity.java
@@ -493,6 +493,8 @@ public class CameraActivity extends BaseActivity implements View.OnClickListener
                         }
                         if(params == null){
                             camera.setParameters(params);
+                        }else if( params != null){
+                            camera.setParameters(params);
                         }
 
                     }
