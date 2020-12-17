d15906f72d51b5c481c3f72703137a341c32f7b1
Your Name <you@example.com>
2020-12-17 10:20:14 +0530

    changes on static value in main activity

--- app/src/main/java/com/example/aurosampleapplication/MainActivity.java
+++ app/src/main/java/com/example/aurosampleapplication/MainActivity.java
@@ -176,10 +176,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void openGenricSDK(String mobileNumber, String student_class) {
         AuroScholarInputModel inputModel = new AuroScholarInputModel();
         inputModel.setMobileNumber(mobileNumber);//7503600601
-        inputModel.setStudentClass(student_class);
-        inputModel.setRegitrationSource("AuroScholr");
-        inputModel.setReferralLink("https://rb.gy/np9uh5");
-        inputModel.setPartnerSource("AURO3VE4j7"); //this id is provided by auroscholar for valid partner//Demo partner id:AUROJ1i5dA
+        inputModel.setStudentClass("put student class here");
+        inputModel.setRegitrationSource("put scholarId here");
+        inputModel.setReferralLink("put refferal link");
+        inputModel.setPartnerSource("put regitration source"); //this id is provided by auroscholar for valid partner//Demo partner id:AUROJ1i5dA
         inputModel.setUserPartnerId("TestUserpartnerId");
         inputModel.setActivity(this);
         inputModel.setFragmentContainerUiId(R.id.home_container);
