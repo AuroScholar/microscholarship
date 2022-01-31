9e2e276a6fc31259c4cdbcd06bc6c9bdfc7365a2
pradeep <auroscholar@gmail.com>
2022-01-31 15:11:15 +0530

    remove code of ppather source

--- app/src/main/java/com/example/aurosampleapplication/MainActivity.java
+++ app/src/main/java/com/example/aurosampleapplication/MainActivity.java
@@ -117,12 +117,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void openTeacherSDK() {
         AuroScholarDataModel auroScholarDataModel = new AuroScholarDataModel();
         auroScholarDataModel.setMobileNumber(binding.mobileNumber.getText().toString());//Mandatory
-        auroScholarDataModel.setRegitrationSource("Auroscholar");
+        auroScholarDataModel.setRegitrationSource("");
         auroScholarDataModel.setShareType("");
         auroScholarDataModel.setShareIdentity("");
         auroScholarDataModel.setActivity(this); // Activity context here
         auroScholarDataModel.setEmailVerified(true);
-        auroScholarDataModel.setPartnerSource("AURO3VE4j7");
+        auroScholarDataModel.setPartnerSource("");
         auroScholarDataModel.setUTMLink(binding.utmTextHere.getText().toString());
        // auroScholarDataModel.setUTMLink("");
 
@@ -169,9 +169,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         AuroScholarInputModel inputModel = new AuroScholarInputModel();
         inputModel.setMobileNumber(mobileNumber);//mobileNumber
         inputModel.setStudentClass(student_class);//"binding.userClass.getText().toString()"
-        inputModel.setRegitrationSource("Auroscholar");
+        inputModel.setRegitrationSource("");
         inputModel.setReferralLink("");
-        inputModel.setPartnerSource("AURO3VE4j7"); //this id is provided by auroscholar for valid partner//Demo partner id:AUROJ1i5dA
+        inputModel.setPartnerSource(""); //this id is provided by auroscholar for valid partner//Demo partner id:AUROJ1i5dA
         inputModel.setActivity(this);
         inputModel.setLanguage(binding.language.getText().toString());
         inputModel.setPartnerLogoUrl(""); //Mandatory
